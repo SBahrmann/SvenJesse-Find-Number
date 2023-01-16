cc61a5e69c6db23d75ceb34fa14b6ad8f59786c3
Steffen Bahrmann+ <s.bahrmann@gmail.com>
2023-01-16 13:38:23 +0100

    Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'svenCrossing';
+
+  combinations: string[] = [];
+  major = 1;
+  minor = 1;
+  patch = 1;
+  value: string | undefined = undefined;
+  result: number | undefined = undefined;
+  index: number | undefined = undefined;
+
+  constructor() {
+    this.combinations = [];
+
+    let str = '';
+
+    for (let i = 1; i <= 6; i++) {
+      for (let j = 1; j <= 6; j++) {
+        for (let k = 1; k <= 6; k++) {
+          this.combinations.push(`${i}.${j}.${k}`);
+        }
+      }
+    }
+  }
+
+  search() {
+    this.value = `${this.major}.${this.minor}.${this.patch}`;
+    this.result = this.major * this.minor * this.patch;
+    this.index = this.combinations.findIndex(val => val === this.value) + 1;
+  }
+
+  getResult(combi: string) {
+    const combiSplit = combi.split('.');
+    return Number(combiSplit[0]) * Number(combiSplit[1]) * Number(combiSplit[2]);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    FlexLayoutModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
